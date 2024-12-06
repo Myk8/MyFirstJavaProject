593348a808fc503e1ea153295bd6b5faac1e1a37
Myk8 <mikeymazza0802@gmail.com>
2024-12-06 22:48:18 +0100

    Update tutorial project 0.9 tutorial

--- src/PushingProjectToGit.java
+++ src/PushingProjectToGit.java
@@ -1,22 +1,36 @@
 public class PushingProjectToGit {
     public static void main(String[] args) {
       /*GIT USE
-     1. VCS/Enable Version CONTROL INTEGRATION
+     A)
+     1. Only do this once when you create new project!
+     "VCS"/"Enable Version CONTROL INTEGRATION"
 
      2.  Git/Commit "CTRL+K"  ---> tick all "Un versioned files"
      3.  "Commit Message" anything ---> press "commit"
-     4. Down left press "Version Control" ---> right click "master"/New Branch for the Master/ name it "main"
 
-     5. Go to your GitHub Account ---> Create new Repository
+     4.Only do this once!
+     Down left press "Version Control" ---> right click "master"/New Branch for the Master/ name it "main"
+
+     5. Only do this once!  Go to your GitHub Account ---> Create new Repository
      / "Owner"/"Repository name": any name / "Description" same name / tick "Add ReadMe File" / "Create Repository"
 
-     6.Go to  "Code"/"HTTPS" and COPY
-     7 GO BACK Idea press the "green tick" means push/"define remote"/paste url
-      if you get error go to press "push"  then modify text on left up corner "main -->origin: master"/
+     6.Only do this once!  Go to  "Code"/"HTTPS" and COPY
+
+     7.0 GO BACK Idea press the "green tick" means "push"/"define remote"/
+     7.1 Only do this once! paste url
+     7.2 Only do this once! if you get error go to press "push"  then modify text on left up corner "main -->origin: master"/
+   ----------------------------------------------------
+
+      Then repeat that when you want to  your and files changes
+      B)
+       2.  Git/Commit "CTRL+K"  ---> tick all "Un versioned files"
+       3.  "Commit Message" anything ---> press "commit"
+       7.0 press the "green tick" means "push"/"define remote"/
        */
 
 
         System.out.println("Hello I am writing from Git");
+
 
     }
 }
