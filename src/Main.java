cc0bb3582215bfff54718af52016fffd87966af7
Myk8 <mikeymazza0802@gmail.com>
2024-12-06 21:18:13 +0100

    Update tutorial project 0.1

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,16 @@ public class Main {
 
      2.  Git/Commit "CTRL+K"  ---> tick all "Un versioned files"
      3.  "Commit Message" anything ---> press "commit"
+     4. Down left press "Version Control" ---> right click "master"/New Branch for the Master/ name it "main"
+
+     5. Go to your GitHub Account ---> Create new Repository/ "Owner"/"Repository name": any name / "Description" same name / tick "Add ReadMe File" / "Create Repository"
+
+     6. Code/HTTPS COPY
+     7 GO BACK INTELIJ
 
         */
+
+
         System.out.println("Hello I am writing from Git");
 
     }
