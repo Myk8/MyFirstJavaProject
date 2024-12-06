87eb6f33ee42e192d78e321421db501a65d04789
Myk8 <mikeymazza0802@gmail.com>
2024-12-06 21:05:00 +0100

    Update tutorial project 0.0

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+      /*GIT USE
+     1. VCS/Enable Version CONTROL INTEGRATION
+
+     2.  Git/Commit "CTRL+K"  ---> tick all "Un versioned files"
+     3.  "Commit Message" anything ---> press "commit"
+
+        */
+        System.out.println("Hello I am writing from Git");
+
+    }
+}
