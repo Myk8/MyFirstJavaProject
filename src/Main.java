8d2ed9435e685a0f6ba015a54af15223c628f689
Myk8 <mikeymazza0802@gmail.com>
2024-12-06 21:29:59 +0100

    Update tutorial project 0.4

--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,13 @@ public class Main {
      3.  "Commit Message" anything ---> press "commit"
      4. Down left press "Version Control" ---> right click "master"/New Branch for the Master/ name it "main"
 
-     5. Go to your GitHub Account ---> Create new Repository/ "Owner"/"Repository name": any name / "Description" same name / tick "Add ReadMe File" / "Create Repository"
+     5. Go to your GitHub Account ---> Create new Repository
+     / "Owner"/"Repository name": any name / "Description" same name / tick "Add ReadMe File" / "Create Repository"
 
-     6. Code/HTTPS COPY
-     7 GO BACK Idea press  pick "push"/"define remote"/paste url
-
-        */
+     6.Go to  "Code"/"HTTPS" and COPY
+     7 GO BACK Idea press the "green tick" means push/"define remote"/paste url
+      if you get error go to press "push"  then modify text on left up corner "main -->origin: master"/
+       */
 
 
         System.out.println("Hello I am writing from Git");
